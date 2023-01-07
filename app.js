f93360790485716d078102d7f1826fc179e6adbb
Gabriel <bar120bosa@gmail.com>
2023-01-07 12:03:22 -0300

    console log alterado

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 consolo.log("Rodando o sistemas de gerenciamento de jogos");
 consle.log("Aplicação está se conectando ao banco de dados");
+
+console.log("O Guilherme fes esse commit sem voce ver!!");
+console.log("")
