839188d023404be4b1106e1a24004acb44d63e8c
Gabriel <bar120bosa@gmail.com>
2023-01-07 12:12:34 -0300

    adicionando um console Log

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ consolo.log("Rodando o sistemas de gerenciamento de jogos");
 consle.log("Aplicação está se conectando ao banco de dados");
 
 console.log("O Guilherme fes esse commit sem voce ver!!");
-console.log("")
+console.log("Enviando para o GitHub")
