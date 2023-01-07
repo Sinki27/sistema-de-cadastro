8491a328f93002c2e2b5b7261ffe5ab777c8d03a
Gabriel <bar120bosa@gmail.com>
2023-01-07 12:41:34 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ consolo.log("Rodando o sistemas de gerenciamento de jogos");
 consle.log("Aplicação está se conectando ao banco de dados");
 
 console.log("O Guilherme fes esse commit sem voce ver!!");
-console.log("")
